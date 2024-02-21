61efe1b0a578be486a3a81bbf32e2f904165bf29
Uriel Carvajalino GIT <uriel_carva@hotmail.com>
2024-02-21 15:52:45 -0500

    inicio del proyecto

---
+++ Index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8" />
+    <title>Login - Sistema de Usuarios</title>
+
+    <meta name="description" content="User login page" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+
+    <style>
+        body {
+            background: #ffe259;
+            background: linear-gradient(to right, hsl(80, 10%, 94%), #ffe259);
+        }
+
+        .bg {
+            background-image: url(img/bg.jpg);
+            background-position: center center;
+            background-size: 100%;
+
+        }
+    </style>
+</head>
+
+<body class="login-layout ">
+    <div class="container w-75 bg-warning mt-5 rounded shadown ">
+        <div class="row align-items-stretch shadow-lg p-3 mb-5 bg-body rounded">
+            <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 "></div>
+            <div class="col bg-white p-5 rounded-end">
+                <div class="text-end">
+                    <img src="./img/logo.avif" width="90" />
+                </div>
+                <h2 class="fw-bold text-center py-5">Bienvenido</h2>
+
+                <!--Mensaje error -->
+             
+
+                <?php
+                if (isset($_GET['error'])) {
+                ?>
+                    <p class="error alert alert-danger">
+                        <?php
+                        echo $_GET['error']
+                        ?>
+
+                    </p>
+                <?php
+                }
+                ?>
+
+              
+
+
+                <!--Login-->
+                <form action="IniciarSesion.php" method="POST">
+
+
+
+
+                    <div class="mb-4">
+                        <label for="text" class="form-label">Usuario</label>
+                        <input type="text" name="Usuario" placeholder="Nombre de usuario" class="form-control">
+                    </div>
+                    <div class="mb-4">
+
+                        <label for="password" class="form-label">Contraseña</label>
+                        <input type="password" name="Clave" placeholder="Clave" class="form-control">
+
+                    </div>
+                    <div class="d-grid">
+
+                        <button type="submit" class="btn btn-dark">Iniciar Sesion</button>
+                    </div>
+                    <div class="my-3">
+                        <a href="">Recuperar Contraseña</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ iniciosesiondb.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-07-2023 a las 18:13:58
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `iniciosesiondb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `Id` int(11) NOT NULL,
+  `Usuario` varchar(20) NOT NULL,
+  `Clave` varchar(20) NOT NULL,
+  `Nombre_Completo` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`Id`, `Usuario`, `Clave`, `Nombre_Completo`) VALUES
+(1, 'uriel', '123456', 'uriel carvajalino');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
