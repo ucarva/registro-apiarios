5e52b92d5d69b4f1121ca050292176f2139c04fc
Uriel Carvajalino <uriel_carva@hotmail.com>
2024-04-04 16:00:37 -0500

    validación de registro apiario

--- startbootstrap-sb-admin-gh-pages/apiario.php
+++ startbootstrap-sb-admin-gh-pages/apiario.php
@@ -1,9 +1,9 @@
 <?php
-    include("registroapiario.php");
+    include("conexion.php");
     if(isset($_POST['register'])){
 
             if(
-                strlen ($_POST['apiario']) >=1 &&
+                strlen ($_POST['nombre']) >=1 &&
                 strlen ($_POST['lugar']) >=1 &&
                 strlen ($_POST['finca']) >=1 &&
                 strlen ($_POST['granjero']) >=1 &&
@@ -12,30 +12,30 @@
             
             ){
 
-                $apiario = trim($_POST['apiario']);
+                $nombre = trim($_POST['nombre']);
                 $lugar = trim($_POST['lugar']);
                 $finca = trim($_POST['finca']);
                 $granjero = trim($_POST['granjero']);
                 $telefono= trim($_POST['telefono']);
                 $fecha = trim($_POST['fecha']);
                 /*$fecha = date("d/m/y");*/
-                $consulta = "INSERT INTO datos(apiario,lugar,finca,granjero,telefono,fecha)
-                    VALUES('$apiario','$lugar','$finca','$granjero','$telefono','$fecha')";
+                $consulta = "INSERT INTO apiarios(nombre,lugar,finca,granjero,telefono,fecha)
+                    VALUES('$nombre','$lugar','$finca','$granjero','$telefono','$fecha')";
                 $resultado = mysqli_query($conexion,$consulta);
                     if($resultado){
                         ?>
                        
-                        <h3 class="success">Tu registro se ha completado</h3>
+                        <h2 class="success">Tu registro se ha completado</h2>
                         <?php
 
                     }else{
                         ?>
-                        <h3 class="error">Ocurrio un error</h3>
+                        <h2 class="danger">Ocurrio un error</h2>
                         <?php
                  }
                 }else{
                         ?>
-                        <h3 class="error">llena todo los campos</h3>
+                        <h2 class="error">llena todo los campos</h2>
                         <?php
                     }
 
