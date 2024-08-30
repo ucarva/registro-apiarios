f21316b3da6eb911aa2165441c0822ae0f4c0a08
Uriel Carvajalino <uriel_carva@hotmail.com>
2024-08-30 09:05:43 -0500

    responsibe menu

--- layoutAdmin/Reportes/reportes.php
+++ layoutAdmin/Reportes/reportes.php
@@ -37,26 +37,28 @@ if (!isset($_SESSION['nombre'])) {
 
 <body class="sb-nav-fixed">
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3 d-none d-lg-block" href="index.php">Montaña Dorada</a>
-        <!-- Sidebar Toggle-->
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-        <!-- Navbar Search-->
-        <form class="form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-    <!-- Navbar -->
+    <!-- Navbar Brand -->
+    <a class="navbar-brand ps-3 d-none d-lg-block" href="index.php">Montaña Dorada</a>
+
+    <!-- Sidebar Toggle (alineado a la izquierda) -->
+    <div class="d-flex">
+        <button class="btn btn-link btn-sm me-4 me-lg-0" id="sidebarToggle" href="#!">
+            <i class="fas fa-bars"></i>
+        </button>
+    </div>
+
+    <!-- Menú desplegable de usuario (alineado a la derecha) -->
     <ul class="navbar-nav ms-auto">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="fas fa-user fa-fw"></i>
             </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="../Index.php">Cerrar sesión</a></li>
+                <li><a class="dropdown-item" href="../index.php">Cerrar sesión</a></li>
             </ul>
         </li>
     </ul>
-</form>
-
-    </nav>
+</nav>
     <div id="layoutSidenav">
         <div id="layoutSidenav_nav">
             <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
